1cee384d5fc9df031547a8fc9ace7b62f02aae5a
AnomolyAvery <ashawsolutions@gmail.com>
2022-08-18 12:26:26 -0400

    Updated file list details

--- src/pages/vendor/products/[id]/files.tsx
+++ src/pages/vendor/products/[id]/files.tsx
@@ -76,11 +76,11 @@ const ProductFiles = () => {
                                         <div className="sm:flex">
                                             <p className="flex items-center text-sm text-gray-500">
                                                 <FaCloudDownloadAlt className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                                1.0.0
+                                                {file.version}
                                             </p>
                                             <p className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
                                                 <FaFileDownload className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                                0
+                                                {file.downloads}
                                             </p>
                                         </div>
                                         <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
